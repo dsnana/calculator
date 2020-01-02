c829e6fc076404d8294242939ecdb1a4464b6c9e
ganesh kalbande <ganeshk7496@gmail.com>
2020-01-02 16:54:05 +0530

    updates in numeric button

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ function App() {
   return (
     <div className="App">
       {/* <NeonButton name={"Submit"}/> */}
-      <NumericButton name7={"7"}/>
-      <NumericButton name8={"8"}/>
-      <NumericButton name9={"9"}/>
-      <NumericButton name4={"4"}/>
-      <NumericButton name5={"5"}/>
-      <NumericButton name6={"6"}/>
-      <NumericButton name1={"1"}/>
-      <NumericButton name2={"2"}/>
-      <NumericButton name3={"3"}/>
-      <NumericButton name0={"0"}/>
+       <NumericButton name={"7"}/>
+        <NumericButton name={"8"}/>
+       <NumericButton name={"9"}/>
+      <NumericButton name={"4"}/>
+      <NumericButton name={"5"}/>
+      <NumericButton name={"6"}/>
+      <NumericButton name={"1"}/>
+      <NumericButton name={"2"}/>
+      <NumericButton name={"3"}/>
+      <NumericButton name={"0"}/>
 
     </div>
-    
+
   );
 }
 
