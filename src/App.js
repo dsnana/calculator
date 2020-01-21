49658be59ab635dc63c1f817a631e603021f5b98
anilrajole <rajoleanil75@gmail.com>
2020-01-21 17:32:46 +0530

    Before Starting Sum Component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import NeonButton from "./components/NeonButton";
 import NumericButton from "./components/NumericButton";
 import "./App.css";
+import {Button} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      {/* <NeonButton name={"Submit"}/> */}
-       <NumericButton name={"7"}/>
-        <NumericButton name={"8"}/>
-       <NumericButton name={"9"}/>
-      <NumericButton name={"4"}/>
-      <NumericButton name={"5"}/>
-      <NumericButton name={"6"}/>
-      <NumericButton name={"1"}/>
-      <NumericButton name={"2"}/>
-      <NumericButton name={"3"}/>
-      <NumericButton name={"0"}/>
+      {/*<Button> Click</Button>*/}
+      {/*<Button >Hi</Button>*/}
+      {/* <NeonButton name={"Submit"}/>*/}
+      {/* <NumericButton name={"7"}/>*/}
+      {/*  <NumericButton name={"8"}/>*/}
+      {/* <NumericButton name={"9"}/>*/}
+      {/*<NumericButton name={"4"}/>*/}
+      {/*<NumericButton name={"5"}/>*/}
+      {/*<NumericButton name={"6"}/>*/}
+      {/*<NumericButton name={"1"}/>*/}
+      {/*<NumericButton name={"2"}/>*/}
+      {/*<NumericButton name={"3"}/>*/}
+      {/*<NumericButton name={"0"}/>*/}
 
     </div>
 
