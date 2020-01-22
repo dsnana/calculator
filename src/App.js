55489b3e7da6b3289c56b56ca92d0c478e32d5dc
anilrajole <rajoleanil75@gmail.com>
2020-01-22 11:46:10 +0530

    Complete sample module of Addition of two number

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import NeonButton from "./components/NeonButton";
-import NumericButton from "./components/NumericButton";
 import "./App.css";
-import {Button} from "react-bootstrap";
+import Sum from "./components/Sum";
 
 function App() {
   return (
-    <div className="App">
-      {/*<Button> Click</Button>*/}
-      {/*<Button >Hi</Button>*/}
-      {/* <NeonButton name={"Submit"}/>*/}
-      {/* <NumericButton name={"7"}/>*/}
-      {/*  <NumericButton name={"8"}/>*/}
-      {/* <NumericButton name={"9"}/>*/}
-      {/*<NumericButton name={"4"}/>*/}
-      {/*<NumericButton name={"5"}/>*/}
-      {/*<NumericButton name={"6"}/>*/}
-      {/*<NumericButton name={"1"}/>*/}
-      {/*<NumericButton name={"2"}/>*/}
-      {/*<NumericButton name={"3"}/>*/}
-      {/*<NumericButton name={"0"}/>*/}
-
-    </div>
-
+      <Sum/>
   );
 }
 
